d59abde6fc0e713b15d80e558e19a50c50ccd4f3
marcus-rubcube <marcus.vinicius@rubcube.com>
2024-04-28 18:09:54 -0300

    feat: insert on btree

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -1,2 +1,12 @@
-public class Aplicacao {
+public class Aplicacao
+{
+    public static void main(String[] args) {
+        BTree b = new BTree();
+
+        for(int i=1; i<=50000000; i++)
+            b.inserir(i, 0);
+        System.out.println("Terminou!!!");
+
+        b.in_ordem();
+    }
 }
--- src/BTree.java
+++ src/BTree.java
@@ -1,2 +1,123 @@
-public class BTree {
+public class BTree
+{
+    private No raiz;
+
+    public BTree()
+    {
+        raiz = null;
+    }
+
+    private No navegarAteFolha(int info)
+    {
+        No atual = raiz;
+        int pos;
+        while (atual.getvLig(0)!=null)
+        {
+            pos = atual.procurarPosicao(info);
+            atual = atual.getvLig(pos);
+        }
+        return atual;
+    }
+
+    private No localizarPai(No folha, int info)
+    {
+        No atual = raiz;
+        No pai = atual;
+        int pos;
+        while(atual != folha)
+        {
+            pai = atual;
+            pos = atual.procurarPosicao(info);
+            atual = atual.getvLig(pos);
+        }
+        return pai;
+    }
+
+    private void split(No folha, No pai)
+    {
+        No cx1 = new No();
+        No cx2 = new No();
+        for(int i=0; i<No.m; i++)
+        {
+            cx1.setvInfo(i, folha.getvInfo(i));
+            cx1.setvPos(i, folha.getvPos(i));
+            cx1.setvLig(i, folha.getvLig(i));
+        }
+        cx1.setvLig(No.m, folha.getvLig(No.m));
+        cx1.setTl(No.m);
+
+        for(int i=No.m+1; i<2*No.m+1 ; i++)
+        {
+            cx2.setvInfo(i-(No.m+1), folha.getvInfo(i));
+            cx2.setvPos(i-(No.m+1), folha.getvPos(i));
+            cx2.setvLig(i-(No.m+1), folha.getvLig(i));
+        }
+        cx2.setvLig(No.m, folha.getvLig(2*No.m+1));
+        cx2.setTl(No.m);
+
+        if(pai==folha)
+        {
+            folha.setvInfo(0, folha.getvInfo(No.m));
+            folha.setvPos(0, folha.getvPos(No.m));
+            folha.setTl(1);
+            folha.setvLig(0, cx1);
+            folha.setvLig(1, cx2);
+        }
+        else
+        {
+            int pos = pai.procurarPosicao(folha.getvInfo(No.m));
+            pai.remanejar(pos);
+            pai.setvInfo(pos, folha.getvInfo(No.m));
+            pai.setvPos(pos, folha.getvPos(No.m));
+            pai.setTl(pai.getTl()+1);
+            pai.setvLig(pos, cx1);
+            pai.setvLig(pos+1, cx2);
+            if(pai.getTl()>2*No.m)
+            {
+                folha=pai;
+                pai=localizarPai(folha, folha.getvInfo(0));
+                split(folha, pai);
+            }
+        }
+    }
+
+    public void inserir(int info, int posArq)
+    {
+        No folha, pai;
+        int pos;
+        if(raiz == null)
+            raiz = new No(info,posArq);
+        else
+        {
+            folha = navegarAteFolha(info);
+            pos = folha.procurarPosicao(info);
+            folha.remanejar(pos);
+            folha.setvInfo(pos, info);
+            folha.setvPos(pos, posArq);
+            folha.setTl(folha.getTl() + 1);
+            if(folha.getTl() > 2*No.m)
+            {
+                pai = localizarPai(folha, info);
+                split(folha, pai);
+            }
+        }
+    }
+
+    public void in_ordem()
+    {
+        in_ordem(raiz);
+    }
+
+    private void in_ordem(No no)
+    {
+        if(no!=null)
+        {
+            for(int i=0; i<no.getTl(); i++)
+            {
+                in_ordem(no.getvLig(i));
+                System.out.println(no.getvInfo(i));
+            }
+            in_ordem(no.getvLig(no.getTl()));
+        }
+    }
 }
